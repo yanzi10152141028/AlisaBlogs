38d80ad0ccddad11df9408e212c38c8bb68d0d96
yuanyan <yuanyan@glodog.cn>
2020-05-21 17:21:58 +0800

    代码格式化整理

--- src/view/Faq.vue
+++ src/view/Faq.vue
@@ -1,69 +1,76 @@
 <template>
-    <div class="faq">
-         <top-menu id="3"></top-menu>
-        <el-row>
-            <el-col :span='18'  :offset='3'>
-       <el-breadcrumb separator-class="el-icon-arrow-right">
-            <el-breadcrumb-item >首页</el-breadcrumb-item>
-            <el-breadcrumb-item>常见问题</el-breadcrumb-item>
+  <div class="faq">
+    <top-menu id="3"></top-menu>
+    <el-row>
+      <el-col :span="18" :offset="3">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item>首页</el-breadcrumb-item>
+          <el-breadcrumb-item>常见问题</el-breadcrumb-item>
         </el-breadcrumb>
         <el-row class="faq_content">
-            <el-col :span="24">
-                <ul class="faq_ul">
-                    <li v-for="(item,index) in questionList" :key="index">
-                        <el-row>
-                        <el-col :span="3" :offset="1" >
-                            <div class="left"  :style="{backgroundImage: 'url(../assets/public/images/border.jpg)'}">
-                            <img src="../assets/public/images/tab.jpg"/>
-                           </div>
-                        </el-col>
-                        <el-col :span="20">
-                          <div class="right">
-                            <p>
-                                问题：<span>{{item.question}}</span>
-                            </p>
-                            <p>解决方案：<span>{{item.answer}}</span></p>
-                            <span class="date_time">{{item.addDate}}</span>
-                           </div>
-                        </el-col>
-                        </el-row>
-                    </li>
-                </ul>
-                <el-pagination
-                    @size-change="handleSizeChange"
-                    @current-change="handleCurrentChange"
-                    :current-page="currentPage"
-                    :page-sizes="[5, 10,15]"
-                    :page-size="pageSize"
-                    layout="total, sizes, prev, pager, next, jumper"
-                    :total="totalItem">
-                </el-pagination> 
-            </el-col>
+          <el-col :span="24">
+            <ul class="faq_ul">
+              <li v-for="(item,index) in questionList" :key="index">
+                <el-row>
+                  <el-col :span="3" :offset="1">
+                    <div
+                      class="left"
+                      :style="{backgroundImage: 'url(../assets/public/images/border.jpg)'}"
+                    >
+                      <img src="../assets/public/images/tab.jpg" />
+                    </div>
+                  </el-col>
+                  <el-col :span="20">
+                    <div class="right">
+                      <p>
+                        问题：
+                        <span>{{item.question}}</span>
+                      </p>
+                      <p>
+                        解决方案：
+                        <span>{{item.answer}}</span>
+                      </p>
+                      <span class="date_time">{{item.addDate}}</span>
+                    </div>
+                  </el-col>
+                </el-row>
+              </li>
+            </ul>
+            <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page="currentPage"
+              :page-sizes="[5, 10,15]"
+              :page-size="pageSize"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="totalItem"
+            ></el-pagination>
+          </el-col>
         </el-row>
-         </el-col>
-        </el-row>
+      </el-col>
+    </el-row>
     <bottom-content></bottom-content>
-    </div>
+  </div>
 </template>
 <script>
 import TopMenu from "../components/topMenu";
 import BottomContent from "../components/bottom";
-import moment from 'moment'
+import moment from "moment";
 export default {
   components: { TopMenu, BottomContent },
-  data(){
-      return{
-        currentPage: 1,
-        totalItem: 0,
-        pageSize: 5,
-        index: 1,
-        questionList: [],
-      }
+  data() {
+    return {
+      currentPage: 1,
+      totalItem: 0,
+      pageSize: 5,
+      index: 1,
+      questionList: []
+    };
   },
-  mounted(){
+  mounted() {
     this.getQuestion();
   },
-  methods:{
+  methods: {
     //获取我的问题
     getQuestion() {
       var self = this;
@@ -103,8 +110,8 @@ export default {
         .then(function(data) {
           if (data.status == 200) {
             var time = "",
-                question = "",
-                answer = "";
+              question = "",
+              answer = "";
             for (var i = 0; i < data.data.length; i++) {
               question = data.data[i].question;
               answer = data.data[i].answer;
